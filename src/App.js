33fff88495c184809dc98d52ffce7b26766e693c
nexxbyte <arago.pj18@gmail.com>
2025-12-23 14:38:15 +0800

    splitting redux reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import Counter from './components/Counter';
-
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
+import Auth from "./components/Auth";
 
 function App() {
-  return (
-    <Counter />
-  );
+	const auth = useSelector((state) => state.auth.isAuthenticated);
+
+	return (
+		<Fragment>
+			<Header />
+			{auth ? <UserProfile /> : <Auth />}
+			<Counter />
+		</Fragment>
+	);
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,35 @@
-import classes from './Header.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Header.module.css";
+import { authActions } from "../store/auth";
 
 const Header = () => {
-  return (
-    <header className={classes.header}>
-      <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	const dispatch = useDispatch();
+	const auth = useSelector((state) => state.auth.isAuthenticated);
+
+	const logoutHandler = () => {
+		dispatch(authActions.logout());
+	};
+
+	return (
+		<header className={classes.header}>
+			<h1>Redux Auth</h1>
+			{auth && (
+				<nav>
+					<ul>
+						<li>
+							<a href="/">My Products</a>
+						</li>
+						<li>
+							<a href="/">My Sales</a>
+						</li>
+						<li>
+							<button onClick={logoutHandler}>Logout</button>
+						</li>
+					</ul>
+				</nav>
+			)}
+		</header>
+	);
 };
 
 export default Header;
